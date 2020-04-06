9c18a36df7a3ff0557102b28684d20d057e2b0dc
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-06 18:20:31 -0300

    Update Set-ChaveFiapPermission.ps1

--- Set-ChaveFiapPermission.ps1
+++ Set-ChaveFiapPermission.ps1
@@ -1,14 +1,25 @@
+# https://blog.netwrix.com/2018/04/18/how-to-manage-file-system-acls-with-powershell-scripts/
+
 # Set Variable ::
-Set Key="chave-fiap.pem"
+#Set Key="chave-fiap.pem"
+$KEY = "chave-fiap.pem"
 
 # Remove Inheritance ::
-Cmd /c Icacls %Key% /c /t /Inheritance:d
+#Cmd /c Icacls %Key% /c /t /Inheritance:d
+$acl = Get-Acl $KEY
+$acl.SetAccessRuleProtection($true,$false)
+$acl | Set-Acl $KEY
+
 
 # Set Ownership to Owner ::
-Cmd /c Icacls %Key% /c /t /Grant %UserName%:F
+#Cmd /c Icacls %Key% /c /t /Grant %UserName%:F
+$acl = Get-Acl $KEY
+$object = New-Object System.Security.Principal.Ntaccount("$env:username)
+$acl.SetOwner($object)
+$acl | Set-Acl $KEY
 
 # Remove All Users, except for Owner ::
-Cmd /c Icacls %Key% /c /t /Remove Administrator "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+#Cmd /c Icacls %Key% /c /t /Remove Administrator "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
 
 # Verify ::
 Cmd /c Icacls %Key%
