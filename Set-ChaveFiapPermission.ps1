9d991387d780434056f2a0018ddc697e8b55df05
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-06 18:04:00 -0300

    Update Set-ChaveFiapPermission.ps1

--- Set-ChaveFiapPermission.ps1
+++ Set-ChaveFiapPermission.ps1
@@ -1,14 +1,14 @@
-:: # Set Variable ::
+# Set Variable ::
 Set Key="chave-fiap.pem"
 
-:: # Remove Inheritance ::
+# Remove Inheritance ::
 Cmd /c Icacls %Key% /c /t /Inheritance:d
 
-:: # Set Ownership to Owner ::
+# Set Ownership to Owner ::
 Cmd /c Icacls %Key% /c /t /Grant %UserName%:F
 
-:: # Remove All Users, except for Owner ::
+# Remove All Users, except for Owner ::
 Cmd /c Icacls %Key% /c /t /Remove Administrator "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
 
-:: # Verify ::
+# Verify ::
 Cmd /c Icacls %Key%
