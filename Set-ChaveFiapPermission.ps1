6f8096e0c230490d81f82a16d574c72366f141ea
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-07 09:22:07 -0300

    Update Set-ChaveFiapPermission.ps1

--- Set-ChaveFiapPermission.ps1
+++ Set-ChaveFiapPermission.ps1
@@ -1,9 +1,17 @@
 # https://blog.netwrix.com/2018/04/18/how-to-manage-file-system-acls-with-powershell-scripts/
 # https://superuser.com/questions/1296024/windows-ssh-permissions-for-private-key-are-too-open
+# https://blue42.net/windows/changing-ntfs-security-permissions-using-powershell/
+# https://superuser.com/questions/1524970/removing-users-via-powershell-with-multiple-domains
 
 # Set Variable ::
 #Set Key="chave-fiap.pem"
-$KEY = ".\chave-fiap.pem"
+#$KEY = ".\chave-fiap.pem"
+$KEY = ".\sounds.zip"
+
+Write-Output "============= Permissões iniciais ============="
+# Verify ::
+# Cmd /c Icacls $KEY
+Get-Acl $KEY | Format-List
 
 # Remove Inheritance ::
 #Cmd /c Icacls %Key% /c /t /Inheritance:d
@@ -19,36 +27,35 @@ $object = New-Object System.Security.Principal.Ntaccount("$env:username")
 $acl.SetOwner($object)
 $acl | Set-Acl $KEY
 
+Write-Output "============= Permissões sem Herança e ajustado o Ownership ============="
+# Verify ::
+# Cmd /c Icacls $KEY
+Get-Acl $KEY | Format-List
+
 # Remove All Users, except for Owner ::
 #Cmd /c Icacls $KEY /c /t /Remove Administrator "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
 $acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrator","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("System","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
-$acl | Set-Acl $KEY
-$acl = Get-Acl $KEY
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","Allow")
-$acl.RemoveAccessRule($AccessRule)
+foreach($user in $acl.Access.IdentityReference.Value )
+{
+  Write-Host "  ==  Removendo $user ==  " -ForegroundColor Yellow
+  $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($user,"FullControl","Allow")
+  $acl.RemoveAccessRule($AccessRule)
+  $acl | Set-Acl $KEY
+}
+
+Write-Output "============= Permissões removidas ============="
+# Verify ::
+# Cmd /c Icacls $KEY
+Get-Acl $KEY | Format-List
+
+# Deixar a permissão somente para o proprio usuario
+acl = Get-Acl $KEY
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$env:username","FullControl","Allow")
+$acl.AddAccessRule($AccessRule)
 $acl | Set-Acl $KEY
 
+
+Write-Output "============= Permissões somente para o proprio usuario  ============="
 # Verify ::
-Cmd /c Icacls $KEY
+# Cmd /c Icacls $KEY
+Get-Acl $KEY | Format-List
